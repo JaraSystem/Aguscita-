4616a3c19dbddd4156967bb874f7ce25f5177cff
Efrain Jara <50875474+JaraSystem@users.noreply.github.com>
2026-01-07 23:19:29 -0500

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -162,8 +162,8 @@ function showCountdown() {
 
     container.innerHTML =
       `felices: <b>${days}</b> días mi amor<br>` ;
-    //   `Nuestro aniversario: <b>${eventDays}d ${eventHours}h ${eventMinutes}m ${eventSeconds}s</b>`;
-    // container.classList.add('visible');
+      `Nuestro aniversario: <b>${eventDays}d ${eventHours}h ${eventMinutes}m ${eventSeconds}s</b>`;
+     container.classList.add('visible');
   }
   update();
   setInterval(update, 1000);
